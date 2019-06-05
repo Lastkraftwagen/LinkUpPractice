0176f94f06e90f16f07ded244ed505e55cd1c97d
Jagermeister <miriladon@gmail.com>
2019-06-05 20:12:48 +0300

    one more

--- src/buff.js
+++ src/buff.js
@@ -24,24 +24,40 @@ function moveToSelected(element) {
 
     nextSecond.classList.remove(nextSecond.className);
     nextSecond.classList.add('nextRightSecond');
-    prevSecond.classList.remove(nextSecond.className);
+    prevSecond.classList.remove(prevSecond.className);
     prevSecond.classList.add('prevLeftSecond');
 
-    // $(nextSecond).removeClass().addClass("nextRightSecond");
-    // $(prevSecond).removeClass().addClass("prevLeftSecond");
+    // next = nextAll(nextSecond);
+	// next.forEach(function(el) {
+    //     el.classList.remove(el.className);
+    //     el.classList.add('hideRight');
+	// });
 
-    // $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    // $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+    // prev = prevAll(prevSecond);
+	// prev.forEach(function(el) {
+    //     el.classList.remove(el.className);
+    //     el.classList.add('hideLeft');
+	// });
 
 }
 
-// $('#carousel div').click(function () {
-//     moveToSelected($(this));
-// });
+// function nextAll(elem) {
+//     var next = false;
+//     return [].filter.call(elem.parentNode.children, function(child) {
+//         if (child === elem) next = true;
+//         return next && child !== elem
+//     })
+// };
+// function prevAll(element) {
+//     var result = [];
+//     while (element = element.previousElementSibling)
+//         result.push(element);
+//     return result;
+// };
 
 function prev_click() 
 {
-    moveToSelected('next');
+    moveToSelected('prev');
 };
 
 function next_click() 
