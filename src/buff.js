50f55d66a70c4fe7e99090b4cf50052c7c521057
Jagermeister <miriladon@gmail.com>
2019-06-06 15:52:39 +0300

    кличная карусель в проекте

--- src/buff.js
+++ src/buff.js
@@ -1,110 +1,109 @@
 function moveToSelected(element) {
     const divs = document.getElementById('carousel').children;
     const mass = [...divs];
-    
-    if (element == "next") {
-    } else if (element == "prev") {
-        selected = document.querySelector('.prev');
-    } else {
-        selected = element;
-    }
-
     let index = mass.indexOf(document.querySelector(".current"));
 
-    if(index == mass.length - 1)
-    {
-        hide(mass);
-        mass[2].className = 'rightHide'
-        mass[mass.length - 2].className = 'leftHide';
-
-        mass[0].className = 'current';
-        mass[1].className = 'next';
-        mass[mass.length - 1].className = 'prev';
-        console.log('1st');
-        return;
+    if (element == "next") {
+        if(index == mass.length - 1)
+        {
+            hide(mass);
+            mass[mass.length - 2].className = 'leftHide';
+            mass[mass.length - 1].className = 'prev';
+            mass[0].className = 'current';
+            mass[1].className = 'next';
+            mass[2].className = 'rightHide'
+            return;
+        }
+        else if(index == mass.length - 2)
+        {
+            hide(mass);
+            mass[mass.length - 3].className = 'leftHide';
+            mass[mass.length - 2].className = 'prev';
+            mass[mass.length - 1].className = 'current';
+            mass[0].className = 'next';
+            mass[1].className = 'rightHide'
+            return;
+        }
+        else if(index == 0)
+        {
+            hide(mass);
+            mass[3].className = 'rightHide'
+            mass[mass.length-1].className = 'leftHide';
+            mass[index+1].className = 'current';
+            mass[index].className = 'prev';
+            mass[index+2].className = 'next';
+            mass[index+3].className = 'rightHide';
+            return;
+        }
+        else if(index == mass.length-3)
+        {
+            hide(mass);
+            mass[mass.length - 4].className = 'leftHide';
+            mass[mass.length - 3].className = 'prev';
+            mass[mass.length - 2].className = 'current';
+            mass[mass.length - 1].className = 'next';
+            mass[0].className = 'rightHide'
+            return;
+        }
+        else{
+            console.log('5th');
+            no_hide(mass);
+            mass[index-1].className = 'leftHide';
+            mass[index+1].className = 'current';
+            mass[index].className = 'prev';
+            mass[index+2].className = 'next';
+            mass[index+3].className = 'rightHide';
+        }
+    }else if(element == "prev"){
+        if(index == mass.length - 1)
+        {
+            hide(mass);
+            mass[0].className = 'rightHide'
+            mass[index-3].className = 'leftHide';
+            mass[index].className = 'next';
+            mass[index-2].className = 'prev';
+            mass[index-1].className = 'current';
+            return;
+        }
+        else if(index == 0)
+        {
+            hide(mass);
+            mass[index+1].className = 'rightHide'
+            mass[index].className = 'next';
+            mass[mass.length-1].className = 'current';
+            mass[mass.length-2].className = 'prev';
+            mass[mass.length-3].className = 'leftHide';
+            return;
+        }
+        else if(index == 1)
+        {
+            hide(mass);
+            mass[index+1].className = 'rightHide'
+            mass[index].className = 'next';
+            mass[index-1].className = 'current';
+            mass[mass.length-1].className = 'prev';
+            mass[mass.length-2].className = 'leftHide';
+            return;
+        }
+        else if(index == 2)
+        {
+            hide(mass);
+            mass[index+1].className = 'rightHide'
+            mass[index].className = 'next';
+            mass[index-1].className = 'current';
+            mass[index-2].className = 'prev';
+            mass[mass.length-1].className = 'leftHide';
+            return;
+        }
+        else{
+            no_hide(mass);
+            mass[index+1].className = 'rightHide'
+            mass[index-3].className = 'leftHide';
+            mass[index].className = 'next';
+            mass[index-2].className = 'prev';
+            mass[index-1].className = 'current';
+        }
     }
-    else if(index == mass.length - 2)
-    {
-        hide(mass);
-        mass[1].className = 'rightHide'
-        mass[mass.length - 3].className = 'leftHide';
-
-        mass[mass.length - 1].className = 'current';
-        mass[mass.length - 2].className = 'prev';
-        mass[0].className = 'next';
-        console.log('2nd');
-        return;
-    }
-    else if(index == 0)
-    {
-        hide(mass);
-        mass[3].className = 'rightHide'
-        mass[mass.length - 1].className = 'leftHide';
-
-        mass[0].className = 'prev';
-        mass[1].className = 'current';
-        mass[2].className = 'next';
-        console.log('3nd');
-
-        return;
-    }
-    else if(index == mass.length-3)
-    {
-        hide(mass);
-        mass[0].className = 'rightHide'
-        mass[mass.length - 4].className = 'leftHide';
-
-        mass[mass.length - 3].className = 'prev';
-        mass[mass.length - 2].className = 'current';
-        mass[mass.length - 1].className = 'next';
-        console.log('4th');
-
-        return;
-    }
-    else{
-        console.log('5th');
-        no_hide(mass);
-        let i = mass.indexOf(document.querySelector('.prev'));
-        mass[i].className = 'leftHide';
-        i = mass.indexOf(document.querySelector('.next'));
-        mass[i+2].className = 'rightHide';
-        mass[index+1].className = 'current';
-        mass[index].className = 'prev';
-        mass[index+2].className = 'next';
-    }
-
-    
-    // var next = selected.nextElementSibling;
-    // var prev = selected.previousElementSibling;
-    // var prevSecond = prev.previousElementSibling;
-    // var nextSecond = next.nextElementSibling;
-    
-    // selected.classList.remove(element);
-    // selected.classList.add('selected');
-    
-    // next.classList.remove(next.className);
-    // next.classList.add('next');
-
-    // prev.classList.remove(prev.className);
-    // prev.classList.add('prev');
-
-    // nextSecond.classList.remove(nextSecond.className);
-    // nextSecond.classList.add('nextRightSecond');
-    // prevSecond.classList.remove(prevSecond.className);
-    // prevSecond.classList.add('prevLeftSecond');
-
-    // next = nextAll(nextSecond);
-	// next.forEach(function(el) {
-    //     el.classList.remove(el.className);
-    //     el.classList.add('hideRight');
-	// });
-
-    // prev = prevAll(prevSecond);
-	// prev.forEach(function(el) {
-    //     el.classList.remove(el.className);
-    //     el.classList.add('hideLeft');
-	// });
-
 }
 
 const hide = (mass) => { 
@@ -112,7 +111,6 @@ const hide = (mass) => {
     element.className='hide';
     });
 }
-
 const no_hide = (mass) => { 
     mass.forEach(element => {
         if(element.className != 'prev' && element.className != 'next'){
@@ -120,21 +118,6 @@ const no_hide = (mass) => {
         }
     });
 }
-
-
-// function nextAll(elem) {
-//     var next = false;
-//     return [].filter.call(elem.parentNode.children, function(child) {
-//         if (child === elem) next = true;
-//         return next && child !== elem
-//     })
-// };
-// function prevAll(element) {
-//     var result = [];
-//     while (element = element.previousElementSibling)
-//         result.push(element);
-//     return result;
-// };
 
 function prev_click() 
 {
