da638eb02381a1b3143ac2ce64670bdb24486c4f
Jagermeister <miriladon@gmail.com>
2019-06-07 13:10:38 +0300

    ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-// function change()
-// {
-// document.getElementById('main').innerHTML="New word";
-// }
+
+function showcat()
+{
+    let item = document.getElementById('drop_down_id');
+    item = item.childNodes[1];
+    console.log(item);
+    
+    if(item.className == "fa fa-sort-down")
+    {
+        item.className="fa fa-sort-up";
+    }
+    else
+    {
+        item.className="fa fa-sort-down";
+    }
+    
+};
