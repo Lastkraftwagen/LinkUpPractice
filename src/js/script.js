93dd7e1e0681d2fb4786f0047106ce09984767e2
Jagermeister <miriladon@gmail.com>
2019-06-07 13:59:10 +0300

    finished & edited

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,14 @@
 
-function showcat()
-{
+function showcat() {
     let item = document.getElementById('drop_down_id');
     item = item.childNodes[1];
     console.log(item);
-    
-    if(item.className == "fa fa-sort-down")
-    {
-        item.className="fa fa-sort-up";
+
+    if (item.className == "fa fa-sort-down") {
+        item.className = "fa fa-sort-up";
     }
-    else
-    {
-        item.className="fa fa-sort-down";
+    else {
+        item.className = "fa fa-sort-down";
     }
-    
+
 };
